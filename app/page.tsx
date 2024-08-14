cceca72363593fc66c2c60bc95f5e9b7b28c2030
Zeke <162091817+ZekeVoigt@users.noreply.github.com>
2024-08-14 20:13:28 +0000

    git push

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="bg-white h-screen flex justify-center items-center">
-      <h1>Hello</h1>
+      <h1 className="text-5xl font-semibold">Coming Soon</h1>
     </div>
   );
 }
