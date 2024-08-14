66a48f791418ac68d53f5c2423928aa6e4827acf
Zeke <162091817+ZekeVoigt@users.noreply.github.com>
2024-08-14 22:10:16 +0000

    git push

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,20 @@ export default function Home() {
       <img
         src="/DreamLogo.png"
         alt="Dream Logo"
-        className="mt-4 mb-8 max-w-full h-auto"
+        height={150}
+        width={150}
+        className="mt-4 max-w-full h-auto"
       />
 
       {/* Centered content */}
-      <div className="flex flex-col justify-center items-center flex-grow">
-        <h1 className="text-7xl font-semibold text-black">Dream</h1>
-        <p className="text-2xl mt-4 text-black">
+      <div className="flex flex-col justify-center items-center mt-52">
+        <h1 className="text-7xl text-black font-Inter">Dream</h1>
+        <p className="text-2xl mt-4 text-gray-600 text-center">
           Creating The Next Generation Of Internet Entrepreneurs
         </p>
+        <button className="bg-black text-white border rounded-3xl h-11 w-[24rem] mt-3">
+          Launching Beta Auguest 18th, 12PM Est.
+        </button>
       </div>
     </div>
   );
