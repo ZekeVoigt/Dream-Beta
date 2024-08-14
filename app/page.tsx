4ba3476e2f21a8fb1865e1a54326f1b94e63a81c
Zeke <162091817+ZekeVoigt@users.noreply.github.com>
2024-08-14 21:14:15 +0000

    git push

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 export default function Home() {
   return (
-    <div className="bg-white h-screen flex flex-col justify-center items-center">
-      <h1 className="text-5xl font-semibold text-black">Dream</h1>
-      <p className="text-2xl mt-4 text-black">
-        Creating The Next Generation Of Internet Entrepreneurs
-      </p>
+    <div className="bg-white h-screen flex flex-col items-center">
+      {/* Image at the top */}
+      <img
+        src="/DreamLogo.png"
+        alt="Dream Logo"
+        className="mt-4 mb-8 max-w-full h-auto"
+      />
+
+      {/* Centered content */}
+      <div className="flex flex-col justify-center items-center flex-grow">
+        <h1 className="text-7xl font-semibold text-black">Dream</h1>
+        <p className="text-2xl mt-4 text-black">
+          Creating The Next Generation Of Internet Entrepreneurs
+        </p>
+      </div>
     </div>
   );
 }
