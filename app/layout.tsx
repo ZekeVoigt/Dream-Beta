8352d793ccfb4d06ca839008bf2074d905daebbf
Zeke <162091817+ZekeVoigt@users.noreply.github.com>
2024-08-17 04:12:22 +0000

    git push

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1714729228561426"
+        crossOrigin="anonymous"
+      ></script>
       <body className={inter.className}>{children}</body>
     </html>
   );
